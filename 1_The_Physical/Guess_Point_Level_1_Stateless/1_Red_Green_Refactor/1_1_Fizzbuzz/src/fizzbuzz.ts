24e465e2a985ded9790b8ff00a33303a5c76a234
georgebullock <george.bullock@gmx.de>
2023-03-27 17:14:45 +0200

    FizzBuzz: Add a guard condition to prevent users from calling the function with negative values. Remove error messages from tests to avoid testing implementation details.

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,7 +1,12 @@
 const fizzbuzz = (number: number) => {
   if (number > 100)
     throw new Error(
-      "The parameter was greater than 100. The parameter value must be a number less than 100."
+      "The parameter value was greater than 100. The parameter value must be a number between 1 and 100."
+    );
+
+  if (number < 1)
+    throw new Error(
+      "The parameter value was less than 1. The parameter value must be a number between 1 and 100."
     );
 };
 
--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -5,14 +5,21 @@ describe("fizzbuzz", () => {
   it("should throw an error if the parameter value is greater than 100", () => {
     expect(() => {
       fizzbuzz(101);
-    }).toThrow(
-      "The parameter was greater than 100. The parameter value must be a number less than 100."
-    );
+    }).toThrow();
   });
 
-  // it should throw an error if the parameter value is 0
+  it("should throw an error if the parameter value is 0", () => {
+    expect(() => {
+      fizzbuzz(0);
+    }).toThrow();
+  });
 
   // it should throw an error if the parameter value is less than 1
+  it("should throw an error if the parameter value is 0", () => {
+    expect(() => {
+      fizzbuzz(-1);
+    }).toThrow();
+  });
 
   // it should return fizz for multiples of 3
 
