25234874990b0d1c3372a1a3bd0c8abac8074c2b
georgebullock <george.bullock@gmx.de>
2023-04-04 10:33:27 +0200

    Palindrome: Add capability to normalize parameter value to lowercase, reverse it, compare it to the input parameter value and return the value of the comparison

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -1,8 +1,34 @@
 import { describe, expect, it } from "@jest/globals";
 import { isPalindrome } from "./index";
 
+const palindromes = [
+  { value: "mom" },
+  { value: "Noon" },
+  { value: "ciVic" },
+  { value: "radAR" },
+  { value: "LeveL" },
+];
+
+const nonPalindromes = [
+  { value: "Berlin" },
+  { value: "Munich" },
+  { value: "Hanover" },
+  { value: "Potsdam" },
+  { value: "Frankfurt" },
+];
+
 describe("palindrome checker", () => {
-  it("should return true when the parameter value is a palindrome", () => {
-    expect(isPalindrome("mom")).toBe(true);
-  });
+  it.each(palindromes)(
+    "should return true when the parameter value is a palindrome",
+    (table) => {
+      expect(isPalindrome(table.value)).toBe(true);
+    }
+  );
+
+  it.each(nonPalindromes)(
+    "should should return false when the parameter value is not a palindrome",
+    (table) => {
+      expect(isPalindrome(table.value)).toBe(false);
+    }
+  );
 });
--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
@@ -1,5 +1,13 @@
 const isPalindrome = (value: string) => {
-  return true;
+  // Normalize the input value
+  const normalizedValue = value.toLowerCase();
+
+  // Reverse the value
+  const reversedValue = normalizedValue.split("").reverse().join("");
+
+  // Compare the input and reversed values
+  // Return the value of the comparison
+  return normalizedValue === reversedValue;
 };
 
 export { isPalindrome };
