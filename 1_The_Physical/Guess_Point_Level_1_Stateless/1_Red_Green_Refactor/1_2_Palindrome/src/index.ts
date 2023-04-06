59813d6b9ed09e416ece66c7c32b7af19e84cdd1
georgebullock <george.bullock@gmx.de>
2023-04-06 11:26:55 +0200

    Palindrome: Add comment to clarify string normalization

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
@@ -4,6 +4,7 @@ const isPalindrome = (value: string) => {
       `Problem: The argument is a ${typeof value}. isPalindrome only accepts string arguments. Solution: Change the argument to a string.`
     );
 
+  // Remove the space between words in phrases before transforming them to lowercase
   const normalizedValue = value.split(" ").join("").toLowerCase();
 
   const reversedValue = normalizedValue.split("").reverse().join("");
